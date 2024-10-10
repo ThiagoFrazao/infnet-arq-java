87b1dc01a1e2bc287f71b2142431b968b7d46c94
Thiago da Costa Pinto Frazao de Sousa <f514189@corp.caixa.gov.br>
2024-10-10 20:22:02 -0300

    feat: iniciando projeto e criando gitignore

---
+++ thiago-arq-java/src/test/java/br/infnet/thiagoarqjava/ThiagoArqJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.infnet.thiagoarqjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiagoArqJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
